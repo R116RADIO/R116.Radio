84bb632a2062cfed99071b5b0e5ace085458cc78
Khoa Nguyen <meo.wizard@gmail.com>
2017-03-20 11:33:49 +0700

    fix layout

--- client/modules/home/components/FollowUs.jsx
+++ client/modules/home/components/FollowUs.jsx
@@ -6,7 +6,7 @@ class FollowUs extends Component {
       <div className="home-page__contact--follow-us">
         <h4 className="home-page__contact--follow-us--title">Follow us</h4>
         <ul className="home-page__contact--follow-us--social">
-          <li><i className="fa fa-facebook-official"></i></li>
+          <li><i className="fa fa-facebook-square"></i></li>
           <li><i className="fa fa-twitter-square"></i></li>
           <li><i className="fa fa-youtube"></i></li>
         </ul>
